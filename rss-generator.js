0f3fa3eec017bed5145478a7f85a04ca3278dbcf
إشراق للتنمية المجتمعية | ECD <info.eshraq.org@gmail.com>
2025-06-23 01:25:48 +0300

    Update rss-generator.js
    Update project files with latest content and improvements

--- rss-generator.js
+++ rss-generator.js
@@ -1,37 +1,59 @@
 const fs = require('fs');
 const path = require('path');
 const matter = require('gray-matter');
-const { parseISO, format } = require('date-fns');
+const { format } = require('date-fns');
 
 const postsDir = path.join(__dirname, 'posts');
-const files = fs.readdirSync(postsDir).filter(file => file.endsWith('.md'));
+const feedPath = path.join(__dirname, 'feed.xml');
 
-const items = files.map(file => {
-  const fullPath = path.join(postsDir, file);
-  const content = fs.readFileSync(fullPath, 'utf-8');
-  const { data, content: body } = matter(content);
-  const url = `https://eshraqorg.github.io/posts/${file.replace(/\.md$/, '')}`;
-  const pubDate = data.date ? format(parseISO(data.date), 'EEE, dd MMM yyyy HH:mm:ss xx') : new Date().toUTCString();
-  return `
-    <item>
-      <title>${data.title || file}</title>
-      <link>${url}</link>
-      <guid>${url}</guid>
-      <pubDate>${pubDate}</pubDate>
-      <description><![CDATA[${body.substring(0, 300)}...]]></description>
-    </item>`;
-}).join('\n');
+const siteUrl = 'https://eshraqorg.github.io/posts';
 
-const rss = `<?xml version="1.0" encoding="UTF-8" ?>
+function generateFeed() {
+  const files = fs.readdirSync(postsDir).filter(file => file.endsWith('.md'));
+
+  const items = files.map(filename => {
+    const filePath = path.join(postsDir, filename);
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    const { data, content } = matter(fileContent);
+
+    const title = data.title || filename;
+    
+    let pubDate;
+    try {
+      if (typeof data.date === 'string') {
+        pubDate = new Date(data.date);
+      } else {
+        pubDate = fs.statSync(filePath).ctime;
+      }
+    } catch (e) {
+      pubDate = new Date();
+    }
+
+    const url = `${siteUrl}/${filename.replace('.md', '')}.html`;
+
+    return `
+  <item>
+    <title>${title}</title>
+    <link>${url}</link>
+    <pubDate>${format(pubDate, 'EEE, dd MMM yyyy HH:mm:ss xx')}</pubDate>
+    <guid>${url}</guid>
+    <description><![CDATA[${content.slice(0, 200)}...]]></description>
+  </item>`;
+  }).join('\n');
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0">
 <channel>
   <title>مدونة إشراق</title>
-  <link>https://eshraqorg.github.io/posts</link>
-  <description>مقالات الصحة النفسية والتنمية المجتمعية</description>
+  <link>${siteUrl}</link>
+  <description>تحديث تلقائي للمقالات</description>
   <language>ar</language>
   ${items}
 </channel>
 </rss>`;
 
-fs.writeFileSync(path.join(__dirname, 'feed.xml'), rss);
-console.log('✔ تم إنشاء RSS feed بنجاح.');
+  fs.writeFileSync(feedPath, xml, 'utf8');
+  console.log('✅ تم إنشاء ملف feed.xml بنجاح');
+}
+
+generateFeed();
